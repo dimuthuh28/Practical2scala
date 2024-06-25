2789319214ced216896ebe439520ae84aa045ac6
Dimuthu Harshamal <78658133+dimuthuh28@users.noreply.github.com>
2024-06-25 08:38:41 +0530

    Update 4.scala

--- 4.scala
+++ 4.scala
@@ -1,46 +1,14 @@
-object MovieTheaterProfit {
+object Main extends App{
+  def Cost(p: Int): Int = 500 + Attendence(p)*3
+  def Attendence(p: Int): Int = 120 +(15-p)*4
+  def Revenue(p: Int): Int = Attendence(p)*p
+  def Profit(p: Int): Int = Revenue(p) - Cost(p)
 
-  def calculateProfit(price: Int): Int = {
-    val fixedCostPerPerformance = 500
-    val costPerAttendee = 3
-    val initialPrice = 15
-    val initialAttendance = 120
-    
-    val attendance = price match {
-      case `initialPrice` => initialAttendance
-      case p if p < initialPrice => initialAttendance + 20
-      case p if p > initialPrice => initialAttendance - 20
-    }
-    
-    val revenue = price * attendance
-    val totalCost = fixedCostPerPerformance + (attendance * costPerAttendee)
-    
-    revenue - totalCost
-  }
 
-  def findBestPrice(): (Int, Int) = {
-    val profit1 = calculateProfit(10)
-    val profit2 = calculateProfit(15)
-    val profit3 = calculateProfit(20)
 
-    val maxProfit = List(profit1, profit2, profit3) match {
-      case List(p1, p2, p3) => List(p1, p2, p3).max
-      case _ => 0 
-    }
-   
-    println(s"Profit for Rs 10 ticket price: Rs $profit1")
-    println(s"Profit for Rs 15 ticket price: Rs $profit2")
-    println(s"Profit for Rs 20 ticket price: Rs $profit3")
-
-    val bestPrice = List(10, 15, 20)(List(profit1, profit2, profit3).indexOf(maxProfit))
-
-    (bestPrice, maxProfit)
-  }
-
-  def main(args: Array[String]): Unit = {
-    val (bestPrice, maxProfit) = findBestPrice()
-    println(s"Best ticket price for maximum profit: Rs $bestPrice")
-    println(s"Maximum profit: Rs $maxProfit")
-  }
-
-}
+  println("Ticket Price")
+  println(s"10 => ${Profit(10)}")
+  println(s"15 => ${Profit(15)}")
+  println(s"20 => ${Profit(20)}")
+  println(s"25 => ${Profit(25)}")
+ }
