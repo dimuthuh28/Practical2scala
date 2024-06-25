a0e966ef2cfbb40e66af17356a4e1d73c80d7f7b
Dimuthu Harshamal <78658133+dimuthuh28@users.noreply.github.com>
2024-06-25 08:26:49 +0530

    Update 3.scala

--- 3.scala
+++ 3.scala
@@ -3,7 +3,7 @@ object app{
     def salary(Wh:Int,Oth:Int) : Double = (250 * Wh + 85 * Oth) * (1-0.12); 
 
     def main(args: Array[String]): Unit = {
-        println("Home salary of an employee : Rs " + salary(40,30));
+        println("Take Home salary of an employee : Rs " + salary(40,30));
     }
 
 }
